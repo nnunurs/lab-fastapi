fef85a05af9e3f56db9562a5f3501b918c7e753e
nnunurs <33067782+miszek2000@users.noreply.github.com>
2023-06-06 18:22:49 +0200

    More cleanup

--- app/src/components/DeleteStudent.jsx
+++
@@ -1,7 +0,0 @@
-export default async function deleteStudent(id, update) {
-  fetch(`http://127.0.0.1:8000/students/${id}`, {
-    method: "DELETE",
-  }).then((res) => console.log(res.json()))
-    .catch((error) => console.log(error))
-    .finally(() => update());
-}
